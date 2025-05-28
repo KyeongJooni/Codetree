55bca6c4865db48045517016b506a9410235bfcb
KyeongJoon <165972574+KyeongJooni@users.noreply.github.com>
2025-05-29 00:08:52 +0900

    Title: 합과 평균의 차, Time: 159ms, Memory: 14MB, Status: Wrong Answer - Codetree

---
+++ 250529/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,9 @@
+let num1 = 10;
+let num2 = 3;
+let num3 = 5;
+
+let totalValue = num1 + num2 + num3;
+let averageValue = totalValue / 3;
+let result = totalValue - averageValue;
+
+console.log(`${totalValue}\n${averageValue}\n${result}`);
