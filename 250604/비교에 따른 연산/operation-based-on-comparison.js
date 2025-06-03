1c3cd545f4f08f5193e5c2afb6e9d5d57bd34cc4
KyeongJoon <165972574+KyeongJooni@users.noreply.github.com>
2025-06-04 04:40:48 +0900

    Title: 비교에 따른 연산, Time: 175ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250604/비교에 따른 연산/operation-based-on-comparison.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(" ");
+const N1 = parseInt(input[0]);
+const N2 = parseInt(input[1]);
+if (N1 > N2) {
+  console.log(N1*N2);
+} else {
+  console.log(N1/N2);
+}
