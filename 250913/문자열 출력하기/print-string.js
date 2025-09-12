5c19b8ec75b953c8287c1e75ba0d9f301db75039
KyeongJoon <165972574+KyeongJooni@users.noreply.github.com>
2025-09-13 00:05:06 +0900

    Title: 문자열 출력하기, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250913/문자열 출력하기/print-string.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().trim();
+const N = parseInt(input);
+
+for (let i = 0; i < N; i++) {
+  console.log("LeebrosCode");
+}
